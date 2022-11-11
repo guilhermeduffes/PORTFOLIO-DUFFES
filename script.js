d57c7969364819fb74172565255a5afbed496c12
guilherme <guiduffes@gmail.com>
2022-11-11 14:30:25 -0300

    adicionado cards de projetos, parallax e animação de backgorund

--- script.js
+++ script.js
@@ -6,12 +6,6 @@ const frases = ['Hey! Im Duffes.']
 // var playBtn = document.getElementById('play')
 // var heartbeat = document.getElementById('heartbeat')
 
-const AudioContext = window.AudioContext || 
-window.webkitAudioContext;
-
-const audioContext = new AudioContext();
-console.log(audioContext)
-
 var i = 0
 var j = 0
 var frases2 = []
@@ -58,32 +52,97 @@ function loop() {
   setTimeout(loop, time)
 }
 
-// loop()
+loop()
 
+class parallaxTiltEffect {
 
+  constructor({element, tiltEffect}) {
 
-liNavBar.forEach(li => {
-  li.addEventListener('mouseover', event => {
-    hoverSound.play();
+    this.element = element;
+    this.container = this.element.querySelector(".container");
+    this.size = [300, 360];
+    [this.w, this.h] = this.size;
 
-  }, false);
+    this.tiltEffect = tiltEffect;
+
+    this.mouseOnComponent = false;
+
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    this.defaultStates = this.defaultStates.bind(this);
+    this.setProperty = this.setProperty.bind(this);
+    this.init = this.init.bind(this);
+
+    this.init();
+  }
+
+  handleMouseMove(event) {
+    const {offsetX, offsetY} = event;
+
+    let X;
+    let Y;
+
+    if (this.tiltEffect === "reverse") {
+      X = ((offsetX - (this.w/2)) / 3) / 3;
+      Y = (-(offsetY - (this.h/2)) / 3) / 3;
+    }
+
+    else if (this.tiltEffect === "normal") {
+      X = (-(offsetX - (this.w/2)) / 3) / 3;
+      Y = ((offsetY - (this.h/2)) / 3) / 3;
+    }
+
+    this.setProperty('--rY', X.toFixed(2));
+    this.setProperty('--rX', Y.toFixed(2));
+
+    this.setProperty('--bY', (80 - (X/4).toFixed(2)) + '%');
+    this.setProperty('--bX', (50 - (Y/4).toFixed(2)) + '%');
+  }
+
+  handleMouseEnter() {
+    this.mouseOnComponent = true;
+    this.container.classList.add("container--active");
+  }
+
+  handleMouseLeave() {
+    this.mouseOnComponent = false;
+    this.defaultStates();
+  }
+
+  defaultStates() {
+    this.container.classList.remove("container--active");
+    this.setProperty('--rY', 0);
+    this.setProperty('--rX', 0);
+    this.setProperty('--bY', '80%');
+    this.setProperty('--bX', '50%');
+  }
+
+  setProperty(p, v) {
+    return this.container.style.setProperty(p, v);
+  }
+
+  init() {
+    this.element.addEventListener('mousemove', this.handleMouseMove);
+    this.element.addEventListener('mouseenter', this.handleMouseEnter);
+    this.element.addEventListener('mouseleave', this.handleMouseLeave);
+  }
+
+}
+
+const $ = e => document.querySelector(e);
+
+const wrap1 = new parallaxTiltEffect({
+  element: $('.wrap--1'),
+  tiltEffect: 'reverse'
 });
 
-// liNavBar.forEach(li => {
-//   li.addEventListener('mouseleave', event => {
-//     hoverSound.pause();
-//     hoverSound.currentTime = 0;
-//   }, false);
-// });
+const wrap2 = new parallaxTiltEffect({
+  element: $('.wrap--2'),
+  tiltEffect: 'normal'
+});
 
-
-
-// playBtn.addEventListener('mouseover', function () {
-//   heartbeat.play();
-
-// }, false);
-
-// playBtn.addEventListener('mouseleave', function () {
-//   heartbeat.pause();
-//   heartbeat.currentTime = 0;
-// }, false);
+const wrap3 = new parallaxTiltEffect({
+  element: $('.wrap--3'),
+  tiltEffect: 'reverse'
+});
